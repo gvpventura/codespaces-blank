55c1ee6fbe8218365b83f8bd4dceb94ab9c7a492
gvpventura <gvpventura@gmail.com>
2026-01-09 19:47:49 +0000

    agora vai

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ elif st.session_state.pagina_ativa == "➕ Novo/Editar":
         f_local = st.text_input("Localização (Gaveta/Pasta)", value=aluno_ref.get('localizacao', '')).upper()
         
         opcoes_status = ["", "VIVO", "PERMANENTE"]
-        idx_s = opcoes_status.index(aluno_ref.get('status_arquivo')) if editando and aluno_ref.gegit('status_arquivo') in opcoes_status else 0
+        idx_s = opcoes_status.index(aluno_ref.get('status_arquivo')) if editando and aluno_ref.get('status_arquivo') in opcoes_status else 0
         f_status = st.selectbox("Status", opcoes_status, index=idx_s)
 
         enviar = st.form_submit_button("Atualizar Dados" if editando else "Salvar no Banco")
